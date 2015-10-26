a02ac388b4744dd91f3f56599b87dc367ba2560c
MityaDSCH <mlsgearon@gmail.com>
2015-10-26 15:22:35 -0500

    Added options field/modeling to new-pol page, fix the deleteOption function it's broken

--- client/app/polls/new_poll/new_poll.controller.js
+++ client/app/polls/new_poll/new_poll.controller.js
@@ -1,8 +1,21 @@
 angular.module('fccApp').controller('newPollCtrl', function ($scope, $http, Auth) {
 
 	$scope.currentUser = Auth.getCurrentUser();
-	console.log($scope.getCurrentUser);
+
 	$scope.poll = {};
+	$scope.poll.options = [{}];
+
+	$scope.addOption = function() {
+		$scope.poll.options.push({});
+	};
+
+	$scope.deleteOption = function(num) {
+		console.log(num);
+		if ($scope.poll.options.length > 1) {
+			$(".option").eq(num).remove();
+			$scope.poll.options.splice(num, 1);
+		}
+	};
 
 	$scope.register = function(form) {
 
