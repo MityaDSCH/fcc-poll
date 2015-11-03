0da32ca2cfb6d5cf78d5cf3e157f386087209301
MityaDSCH <mlsgearon@gmail.com>
2015-11-03 13:25:38 -0600

    starts building the user profile page

--- client/app/account/user/user.controller.js
+++ client/app/account/user/user.controller.js
@@ -6,9 +6,18 @@ angular.module('fccApp').controller('userCtrl', function ($scope, $http, Auth, $
 
 	var user = $routeParams.username;
 	$scope.polls = [];
+	$scope.isOwner = false
 	$http.get('/api/users/' + user + '/polls').success(function(item) {
-		console.log(item);
 		$scope.polls = item[0].polls;
 	});
+
+	$scope.deleteBtn = function(num) {
+		console.log(num);
+		$('#delete' + num).removeClass('glyphicon-remove')
+											.text('Are you sure?');
+	};
+
+	//show edit/delete buttons is page belongs to logged in user
+	$scope.isOwner = Auth.getCurrentUser().username === user;
 
 });
