a8216354e011394cf7d1d2414db22b3aed482f75
MityaDSCH <mlsgearon@gmail.com>
2015-11-05 19:06:31 -0600

    makes poll page input form

--- client/app/polls/poll/poll.controller.js
+++ client/app/polls/poll/poll.controller.js
@@ -2,6 +2,20 @@
 
 angular.module('fccApp').controller('pollCtrl', function ($scope, $http, Auth, $routeParams) {
 
+
+	$scope.user = Auth.getCurrentUser();
+	$scope.userId = $scope.user._id || "Anonymous";
+
+	$scope.userVote = '';
+	$scope.setVote = function(option) {
+		if ($scope.poll.voteOptions.indexOf($scope.userVote) != -1) {
+			$scope.poll.voteTotals[$scope.poll.voteOptions.indexOf($scope.userVote)]--;
+		}
+		$scope.poll.voteTotals[$scope.poll.voteOptions.indexOf(option)]++;
+		$scope.userVote = option;
+	};
+
+	//get poll display data
 	$scope.pollId = $routeParams.id;
 	$scope.poll = {};
 	$http.get('/api/polls/' + $scope.pollId).success(function(poll) {
@@ -11,6 +25,9 @@ angular.module('fccApp').controller('pollCtrl', function ($scope, $http, Auth, $
 		$scope.data = $scope.poll.voteTotals;
 		$scope.labels = $scope.poll.voteOptions;
 
+		//activate voted button if user has already voted
+		if ($scope.userId != "Anonymous") {
+			$scope.userVote = $scope.poll.votes[$scope.user.username];
+		}
 	});
-
 });
